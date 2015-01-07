df2f4f81c8e724c258052c557ea06e4d8785142b
xuxujiang00 <xuxujiang0@sina.cn>
2015-01-07 10:04:48 +0800

    test 提交
    范德萨

---
+++ River.ReceiveMail.Helper/ConfigHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace River.ReceiveMail.Helper
+{
+    public static class ConfigHelper
+    {
+        public static string MAIL_Accounts;
+        public static int IntervalTimer;
+        public static string MAIL_FileSavePath;
+
+        static ConfigHelper()
+        {
+            try
+            {
+                MAIL_Accounts = ConfigurationManager.AppSettings["Accounts"];
+                IntervalTimer = int.Parse(ConfigurationManager.AppSettings["IntervalTimer"]);
+                MAIL_FileSavePath = ConfigurationManager.AppSettings["FileSavePath"];
+            }
+            catch
+            {
+                throw new ArgumentException("服务配置错误，请检查config配置文件");
+
+            }
+        }
+    }
+}
---
+++ River.ReceiveMail.WinService/uninstall.bat
@@ -0,0 +1,9 @@
+path %path%;%windir%\Microsoft.NET\Framework\v4.0.30319
+@echo 请按任意键开始卸载云中心消息推送服务程序...
+@echo .
+@pause
+@echo 正在清理原有服务程序...
+InstallUtil River.ReceiveMail.WinService.exe /U > InstallService.log
+@echo .
+@echo 清理完毕，请在 InstallService.log 中查看具体的操作结果。
+@pause
